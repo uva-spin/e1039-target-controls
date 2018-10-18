ac1da5987e7450f29bdd3d695189c458ca38f55a
Joshua Hoskins <jhoskins@jlab.org>
2018-10-18 11:03:38 -0400

    Slow controls packaged together in one place.

---
+++ Magnet-PS-Control/e1039-Magnet-PS-Control-LV13/Mercury iXX Modules/Public/Libraries/Lambda.lvlib
@@ -0,0 +1,31 @@
+﻿<?xml version='1.0' encoding='UTF-8'?>
+<Library LVVersion="13008000">
+	<Property Name="NI.Lib.ContainingLib" Type="Str">Mercury iXX.lvlib</Property>
+	<Property Name="NI.Lib.ContainingLibPath" Type="Str">../../../Mercury iXX.lvlib</Property>
+	<Property Name="NI.Lib.Icon" Type="Bin">%Q#!!!!!!!)!"1!&amp;!!!-!%!!!@````]!!!!"!!%!!!(]!!!*Q(C=\&gt;8"=&gt;MQ%!8143;(8.6"2CVM#WJ",7Q,SN&amp;(N&lt;!NK!7VM#WI"&lt;8A0$%94UZ2$P%E"Y.?G@I%A7=11U&gt;M\7P%FXB^VL\`NHV=@X&lt;^39O0^N(_&lt;8NZOEH@@=^_CM?,3)VK63LD-&gt;8LS%=_]J'0@/1N&lt;XH,7^\SFJ?]Z#5P?=F,HP+5JTTF+5`Z&gt;MB$(P+1)YX*RU2DU$(![)Q3YW.YBG&gt;YBM@8'*\B':\B'2Z&gt;9HC':XC':XD=&amp;M-T0--T0-.DK%USWS(H'2\$2`-U4`-U4`/9-JKH!&gt;JE&lt;?!W#%;UC_WE?:KH?:R']T20]T20]\A=T&gt;-]T&gt;-]T?/7&lt;66[UTQ//9^BIHC+JXC+JXA-(=640-640-6DOCC?YCG)-G%:(#(+4;6$_6)]R?.8&amp;%`R&amp;%`R&amp;)^,WR/K&lt;75?GM=BZUG?Z%G?Z%E?1U4S*%`S*%`S'$;3*XG3*XG3RV320-G40!G3*D6^J-(3D;F4#J,(T\:&lt;=HN+P5FS/S,7ZIWV+7.NNFC&lt;+.&lt;GC0819TX-7!]JVO,(7N29CR6L%7,^=&lt;(1M4#R*IFV][.DX(X?V&amp;6&gt;V&amp;G&gt;V&amp;%&gt;V&amp;\N(L@_Z9\X_TVONVN=L^?Y8#ZR0J`D&gt;$L&amp;]8C-Q_%1_`U_&gt;LP&gt;WWPAG_0NB@$TP@4C`%`KH@[8`A@PRPA=PYZLD8Y!#/7SO!!!!!!</Property>
+	<Property Name="NI.Lib.Version" Type="Str">1.0.0.0</Property>
+	<Property Name="NI.LV.All.SourceOnly" Type="Bool">false</Property>
+	<Item Name="Lambda" Type="Folder">
+		<Item Name="Configuration" Type="Folder">
+			<Item Name="Read" Type="Folder">
+				<Item Name="Lambda Flow Auto (Read).vi" Type="VI" URL="../Lambda/Configuration/Read/Lambda Flow Auto (Read).vi"/>
+				<Item Name="Lambda Percentage Flow (Read).vi" Type="VI" URL="../Lambda/Configuration/Read/Lambda Percentage Flow (Read).vi"/>
+			</Item>
+			<Item Name="Set" Type="Folder">
+				<Item Name="Lambda Flow Auto (Set).vi" Type="VI" URL="../Lambda/Configuration/Set/Lambda Flow Auto (Set).vi"/>
+				<Item Name="Lambda Percentage Flow (Set).vi" Type="VI" URL="../Lambda/Configuration/Set/Lambda Percentage Flow (Set).vi"/>
+			</Item>
+		</Item>
+		<Item Name="Open" Type="Folder">
+			<Item Name="Open Lambda Device.vi" Type="VI" URL="../Lambda/Open/Open Lambda Device.vi"/>
+		</Item>
+		<Item Name="Utility" Type="Folder"/>
+		<Item Name="Signals" Type="Folder">
+			<Item Name="Read" Type="Folder">
+				<Item Name="Lambda Pressure (Read).vi" Type="VI" URL="../Lambda/Signals/Read/Lambda Pressure (Read).vi"/>
+				<Item Name="Lambda Temperature (Read).vi" Type="VI" URL="../Lambda/Signals/Read/Lambda Temperature (Read).vi"/>
+				<Item Name="Lambda Valve Open Percent (Read).vi" Type="VI" URL="../Lambda/Signals/Read/Lambda Valve Open Percent (Read).vi"/>
+			</Item>
+		</Item>
+	</Item>
+</Library>
---
+++ Motor-Tracker/MotorTracker.h
@@ -0,0 +1,104 @@
+#pragma once
+#define EXPORTDLL __declspec(dllexport)
+
+#ifndef motor_tracker_h
+#define motor_tracker_h
+
+void swap(std::vector <double> &pos, std::vector <double> &freq, int i, int j) {
+	double temp_pos = 0;
+	double temp_freq = 0;
+
+	temp_pos = pos[i];
+	temp_freq = freq[i];
+
+	pos[i] = pos[j];
+	freq[i] = freq[j];
+
+	pos[j] = temp_pos;
+	freq[j] = temp_freq;
+
+	return;
+}
+
+EXPORTDLL double MotorTracker(double motor_position, const char *file_path) {
+
+	// *************************************************************
+	//  'file_path' is the path to the map file that is used to 
+	//  convert the motor position to EIO frequency. The path 
+	//  must be written with escape sequences or else windows 
+	//  cannot find the file.
+	// *************************************************************
+
+	// *************************************************************
+	//  Read in file with position/frequency measurement data
+	// *************************************************************
+
+	std::string line;
+	std::string sub1;
+	std::string sub2;
+
+	std::fstream file;
+
+	std::vector <double> position;
+	std::vector <double> frequency;
+
+	bool fNotSorted = true;
+
+	file.open(file_path, std::fstream::in | std::fstream::out);
+	if (!file.is_open()) {
+		std::cerr << "File not opened." << std::endl;
+		exit(1);
+	}
+	while (file.good()) {
+		std::getline(file, line);
+
+		sub1 = line.substr(0, line.find("\t"));
+		sub2 = line.substr(line.find("\t") + 1, line.length());
+
+		position.push_back(atof(sub1.c_str()));
+		frequency.push_back(atof(sub2.c_str()));
+
+	}
+
+	// *************************************************************
+	//  Sort frequency/position data in numerical order
+	// *************************************************************
+
+	double temp_pos = 0;
+	double temp_freq = 0;
+
+	while (fNotSorted) {
+		fNotSorted = false;
+		for (int i = 0; i < position.size() - 1; i++) {
+			if (position[i] > position[i + 1]) {
+				swap(position, frequency, i, i + 1);
+				fNotSorted = true;
+			}
+		}
+	}
+	file.close();
+
+	// *************************************************************
+	//  Find input positiona and extrapolate to determine frequency
+	// *************************************************************
+
+	double slope;
+	double intercept;
+	int lower;
+	int upper;
+
+	for (int i = 0; i < position.size(); i++) {
+		if (position.at(i) > motor_position) {
+			lower = i - 1;
+			upper = i;
+			break;
+		}
+	}
+
+	slope = ((frequency.at(upper) - frequency.at(lower)) / (position.at(upper) - position.at(lower)));
+	intercept = frequency.at(upper) - slope * position.at(upper);
+	double freq_out = slope * motor_position + intercept;
+
+	return(freq_out);
+}
+#endif
